baca6ab916dd2e507b1c62a06ed61874372f6432
muneera9999 <82065470+muneera9999@users.noreply.github.com>
2021-04-07 02:39:44 +0300

    Add files via upload

---
+++ project2.js
@@ -0,0 +1,16 @@
+
+function result() {
+    var score =0;
+    if (document.getElementById('correct1').checked){
+        score++; }
+    if (document.getElementById('correct2').checked){
+        score++; }
+    if (document.getElementById('correct3').checked){
+        score++; }
+    if (document.getElementById('correct4').checked){
+        score++; }
+    if (document.getElementById('correct5').checked){
+        score++; }
+
+  alert("your score is "+score+ "!");
+}
